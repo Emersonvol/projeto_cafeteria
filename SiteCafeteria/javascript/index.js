4a4a1da95d619beda033ab7ead48b4981c93c6c9
Emerson Santos <89021451+Emersonvol@users.noreply.github.com>
2023-08-14 09:48:55 -0300

    Add files via upload

---
+++ SiteCafeteria/javascript/index.js
@@ -0,0 +1,19 @@
+const svg =document.querySelector('.cor')
+svg.checkVisibility
+
+
+
+
+
+
+
+
+
+
+
+
+// const imagens = new Image();
+// imagens.onload = ()=>{
+// svg.innerHTML +=`<img src="${imagens.src}">`
+// }
+// imagens.src = "/svg/heart.svg"
